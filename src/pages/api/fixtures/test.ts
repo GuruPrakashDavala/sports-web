889841b9ee28b4ee56f88033d22754f99ec61c40
Guru Davala <guru.govindaraj@gmail.com>
2023-10-08 17:46:46 +0100

    Added cache age for test endpoint

--- src/pages/api/fixtures/test.ts
+++ src/pages/api/fixtures/test.ts
@@ -4,5 +4,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
+  res.setHeader("Cache-Control", "s-maxage=10");
   res.status(200).json({ name: "John Doe" });
 }
