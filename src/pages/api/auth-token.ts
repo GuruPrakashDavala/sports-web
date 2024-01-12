2e1a094488952bcdacd622e44778d823f51e5d3e
Guru Davala <guru.govindaraj@gmail.com>
2024-01-12 13:07:52 +0000

    api code refactorings

--- src/pages/api/auth-token.ts
+++ src/pages/api/auth-token.ts
@@ -64,7 +64,7 @@ export default async function handler(
         res.status(200).json({ isValid: false });
       }
     } else {
-      res.status(401).json({ isValid: true });
+      res.status(401).json({ isValid: false });
     }
   } catch (err) {
     console.log("Error with API call");
